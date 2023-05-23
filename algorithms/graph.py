6f7a93740f44689b6ca6da433c68341dac7a0c44
Isadora Azevedo <92753238+isazvdd@users.noreply.github.com>
2023-05-23 13:27:26 -0300

    Update graph.py

--- algorithms/graph.py
+++ algorithms/graph.py
@@ -31,18 +31,30 @@ class Graph:
         self.adjacency = {}
 
         for node in self.nodes:
-            self.adjacency[node] = []
+            self.adjacency[node] = {}
 
-        for (a, b) in self.edges:
-            self.adjacency[a].append(b)
-            self.adjacency[b].append(a)
+        for a, b, weight in self.edges:
+            if weight.isdigit():
+                self.adjacency[a][b] = int(weight)
+                self.adjacency[b][a] = int(weight)
+            else:
+                self.adjacency[a][b] = 0
+                self.adjacency[b][a] = 0
 
     def heuristic(self, node_a, node_b):
-        pattern = "^\(([0-9]+),([0-9]+)\)$"
-        info = self.infos[node_a]
+        pattern = r"^\(([0-9]+),([0-9]+)\)$"
+        node_a = int(node_a)
+        if node_a in self.infos:
+            info = self.infos[node_a]
+        else:
+            raise KeyError(f"Nó {node_a} não possui informações.")
         ans = re.fullmatch(pattern, info)
         x1, y1 = [int(i) for i in ans.groups()]
-        info = self.infos[node_b]
+        node_b = int(node_b)
+        if node_b in self.infos:
+            info = self.infos[node_b]
+        else:
+            raise KeyError(f"Nó {node_b} não possui informações.")
         ans = re.fullmatch(pattern, info)
         x2, y2 = [int(i) for i in ans.groups()]
         return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
@@ -107,108 +119,116 @@ class Graph:
         return origin
 
     def astar(self, start, goal):
-        visited = {}
-        origin = {}
+        f_scores = {}
+        g_scores = {}
+        came_from = {}
+
         for node in self.nodes:
-            visited[node] = False
-            origin[node] = None
-        origin[start] = start
+            f_scores[node] = float('inf')
+            g_scores[node] = float('inf')
+            came_from[node] = None
 
-        memory = []
-        self.create_adjacency()
-        self.insert_ordered(memory, start, goal)
-        while memory:
-            node_a = self.remove_nearest(memory, goal)
-            if not visited[node_a]:
-                visited[node_a] = True
-                for node_b in self.adjacency[node_a]:
-                    if not visited[node_b]:
-                        self.insert_ordered(memory, node_b, goal)
-                        origin[node_b] = node_a
-                    if node_b == goal:
-                        return origin
+        f_scores[start] = self.heuristic(start, goal)
+        g_scores[start] = 0
 
-        return origin
+        open_set = [start]
 
-    def __str__(self):
-        content = []
-        content.append("node: %s" % self.nodes)
-        content.append("edge: %s" % self.edges)
-        content.append("info: %s" % self.infos)
-        return '\n'.join(content)
+        while open_set:
+            current = self.remove_nearest(open_set, goal)
+
+            if current == goal:
+                return self.reconstruct_path(came_from, current)
+
+            for neighbor in self.adjacency[current]:
+                tentative_g_score = g_scores[current] + 1
+
+                if tentative_g_score < g_scores[neighbor]:
+                    came_from[neighbor] = current
+                    g_scores[neighbor] = tentative_g_score
+                    f_scores[neighbor] = tentative_g_score + self.heuristic(neighbor, goal)
+                    self.insert_ordered(open_set, neighbor, goal)
+
+        return None
+
+    def reconstruct_path(self, came_from, current):
+        path = []
+        while current is not None:
+            path.insert(0, current)
+            current = came_from[current]
+        return path
 
 
-class TGF:
-    def __init__(self, path=""):
-        self.path = path
-        self.graph = None
+def read_tgf(filename):
+    with open(filename, "r") as file:
+        lines = file.readlines()
 
-    def read_node(self, string, graph):
-        pattern = "^([0-9]+) ?(.*)$"
-        ans = re.fullmatch(pattern, string)
-        (a, info) = ans.groups()
-        graph.add_node(a, info)
+    nodes = {}
+    edges = []
 
-    def read_edge(self, string, graph):
-        pattern = "^([0-9]+) ([0-9]+) ?(.*)$"
-        ans = re.fullmatch(pattern, string)
-        (a, b, info) = ans.groups()
-        graph.add_edge((a, b), info)
+    for line in lines:
+        line = line.strip()
+        if line == "#":
+            break
+        node_id, node_label = line.split()
+        nodes[node_id] = node_label
 
-    def read(self):
-        if os.path.isfile(self.path):
-            handle = open(self.path)
-            lines = handle.read().strip().split('\n')
-            handle.close()
+    for line in lines[len(nodes) + 2:]:
+        line = line.strip()
+        if line == "#":
+            continue
+        node_a, node_b, weight = line.split()
+        edges.append((node_a, node_b, weight))
 
-            self.graph = Graph()
-            read_function = self.read_node
-            for line in lines:
-                line = line.strip()
-                if line == "#":
-                    read_function = self.read_edge
-                else:
-                    read_function(line, self.graph)
+    return Graph(nodes, edges)
 
-        return self.graph
+
+def main():
+    filename = "complete.tgf"
+    complete_graph_tgf = read_tgf(filename)
+
+    start_node = "1"
+    goal_node = "4"
+
+    if start_node not in complete_graph_tgf.nodes:
+        print(f"Nó inicial {start_node} não está presente no grafo.")
+        return
+
+    if goal_node not in complete_graph_tgf.nodes:
+        print(f"Nó objetivo {goal_node} não está presente no grafo.")
+        return
+
+    complete_graph_tgf.create_adjacency()
+
+    dfs_solution = complete_graph_tgf.dfs(start_node, goal_node)
+    dfs_path = get_path(dfs_solution, start_node, goal_node)
+    print("Solução utilizando DFS:")
+    print(dfs_path)
+
+    bfs_solution = complete_graph_tgf.bfs(start_node, goal_node)
+    bfs_path = get_path(bfs_solution, start_node, goal_node)
+    print("Solução utilizando BFS:")
+    print(bfs_path)
+
+    astar_solution = complete_graph_tgf.astar(start_node, goal_node)
+    if astar_solution:
+        astar_path = get_path(astar_solution, start_node, goal_node)
+        print("Solução utilizando A*:")
+        print(astar_path)
+    else:
+        print("Não foi possível encontrar uma solução utilizando A*.")
+
+
+def get_path(solution, start, goal):
+    path = []
+    current = goal
+
+    while current != start:
+        path.insert(0, current)
+        current = solution[current]
+
+    path.insert(0, start)
+    return path
 
 
 if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) > 1:
-        complete_graph_path = "data/complete.tgf"
-        complete_graph_tgf = TGF(complete_graph_path).read()
-        start_node = complete_graph_tgf.nodes[0]
-        goal_node = complete_graph_tgf.nodes[-1]
-
-        print("Solução utilizando DFS:")
-        dfs_solution = complete_graph_tgf.dfs(start_node, goal_node)
-        node = goal_node
-        while node is not None:
-            print(node, complete_graph_tgf.infos[node])
-            if node != dfs_solution[node]:
-                node = dfs_solution[node]
-            else:
-                node = None
-
-        print("\nSolução utilizando BFS:")
-        bfs_solution = complete_graph_tgf.bfs(start_node, goal_node)
-        node = goal_node
-        while node is not None:
-            print(node, complete_graph_tgf.infos[node])
-            if node != bfs_solution[node]:
-                node = bfs_solution[node]
-            else:
-                node = None
-
-        print("\nSolução utilizando A*:")
-        astar_solution = complete_graph_tgf.astar(start_node, goal_node)
-        node = goal_node
-        while node is not None:
-            print(node, complete_graph_tgf.infos[node])
-            if node != astar_solution[node]:
-                node = astar_solution[node]
-            else:
-                node = None
-
+    main()
